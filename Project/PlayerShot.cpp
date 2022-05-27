3b580ba0dd22dcba96a2e91a58d59a888511b25f
yamagisimai <c0431@oic.jp>
2022-05-27 09:32:58 +0900

    9-3

---
+++ Project/PlayerShot.cpp
@@ -0,0 +1,43 @@
+#include "PlayerShot.h"
+
+/**
+ * コンストラクタ
+ */
+CPlayerShot::CPlayerShot() :
+m_pMesh(NULL),
+m_Pos(0.0f,0.0f,0.0f),
+m_bShow(false){
+}
+
+/**
+ * デストラクタ
+ */
+CPlayerShot::~CPlayerShot(){
+}
+
+/**
+ * 初期化
+ */
+void CPlayerShot::Initialize(void){
+}
+
+/**
+ * 発射
+ */
+void CPlayerShot::Fire(const Vector3& p){
+
+}
+
+/**
+ * 更新
+ */
+void CPlayerShot::Update(void){
+
+}
+
+/**
+ * 描画
+ */
+void CPlayerShot::Render(void){
+
+}
